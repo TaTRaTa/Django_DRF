f6bace2d8e2ebc67d110754135fce732f1d63b20
TaTRaTa <cvetomir.defyy@gmail.com>
2020-03-13 00:22:23 +0200

    Setup, Models and Admin

---
+++ env_pollsapi/pollsapi/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\lenovo\desktop\django_drf\env_pollsapi\pollsapi\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
